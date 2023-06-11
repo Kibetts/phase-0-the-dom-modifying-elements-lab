d5c327f6390d634f03649539cd669191f87ad13c
Brian Kibet <kibetsbrian@gmail.com>
2023-06-12 00:32:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+
+var main = document.getElementById("main");
+main.parentNode.removeChild(main);
+
+
+var newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Brian is the champion";
+
+document.body.appendChild(newHeader);
